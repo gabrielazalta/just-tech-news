38d78d448059ffa5d6690e4012e6b725a7170db6
Gabriela Zalta <gzalta96@gmail.com>
2021-01-26 11:38:52 -0500

    security and authentication added

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,25 +49,45 @@ router.post('/', (req, res) => {
     });
 });
 
-//PUT api/users/1
-router.put('/:id', (res, req) => {
-    User.update(req.body, {
+router.post('/login', (req, res) => {
+    User.findOne({
         where: {
-            id: req.params.id
+            email: req.body.email
         }
-    })
-    .then(dbUserData => {
-        if (!dbUserData[0]) {
-            res.status(404).json({ message: 'No user found with this id '});
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
             return;
         }
-        res.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
 });
+
+//PUT api/users/1
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbUserData => {
+          if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
 
 //DELETE api/users/1
 router.delete('/:id', (req, res) => {
